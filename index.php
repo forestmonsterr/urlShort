0fea8b62fa6a1baab64e10992e570089544fdeca
forestmonsterr <alaskayoungmeow@gmail.com>
2023-05-31 21:01:27 +0500

    index up

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php
+session_start();
+?>
 <!DOCKTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>URL Сокращатель</title>
@@ -8,6 +11,12 @@
 <body>
     <div class="container">
         <h1 class="title"> URL Сокращатель</h1>
+        <?php
+        if(isset($_SESSION['feedback'])) {
+            echo "<p>".$_SESSION['feedback']."</p>";
+            unset($_SESSION['feedback']);
+        }
+        ?>
         <form action="php/shorten.php" method="post">
             <input type="url" name="url" placeholder="Введите URL" autocomplete="off">
             <input type="submit" value="Сократить">
