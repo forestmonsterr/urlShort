22f3c46264f966836f0c5ad8ccf92471a683bbf0
forestmonsterr <alaskayoungmeow@gmail.com>
2023-05-31 21:03:32 +0500

    shorten

--- php/shorten.php
+++ php/shorten.php
@@ -1,1 +1,18 @@
 <?php
+session_start();
+require_once "shortener.php";
+
+$s = new Shortener();
+
+if(isset($_POST['url'])){
+    $url = $_POST['url'];
+
+    if($code = $s->makeCode($url)) {
+        $_SESSION['feedback'] = "Ваша ссылка готова: <a
+        href='http://localhost/test/$code'>http://localhost/test/$code</a>";
+    } else {
+        $_SESSION['feedback'] = "Ошибка! Некорректный URL";
+    }
+}
+header('Location: ./index.php');
+?>
