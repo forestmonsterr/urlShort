aca434c6519236d752415eb03fbd427818893f56
forestmonsterr <alaskayoungmeow@gmail.com>
2023-05-31 21:02:44 +0500

    redirect

--- php/redirect.php
+++ php/redirect.php
@@ -1,1 +1,13 @@
 <?php
+require_once "shortener.php";
+
+if(isset($_GET['code'])) {
+    $s = new Shortener();
+    $code = $_GET['code'];
+    if($url = $s->getUrl($code)) {
+        header('Location: {$url}');
+        exit();
+    }
+}
+header("Location: ./index.php");
+?>
